c51a40bcd158ef18ecb91d4528aee69dc28d0732
Odyssey1975 <tire.o@yandex.ru>
2020-06-19 15:38:03 +0300

    spring_helloworld

---
+++ spring-helloworld/src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+  String messagecat;
+
+    public String getMessagecat() {
+        return messagecat;
+    }
+
+    public void setMessagecat(String messagecat) {
+        this.messagecat = messagecat;
+    }
+}
